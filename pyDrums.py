bf8cc139a158161ebf768585fd56e35c12db0959
gabsillis <gianniabsillis@gmail.com>
2015-10-03 11:26:28 -0400

    Update pyDrums.py
    created framework for threading now need to override run from myThread to process each sound in an individual thread. Also changed pygames wait function for the less accurate but less taxing time.sleep()

--- pyDrums.py
+++ pyDrums.py
@@ -1,5 +1,9 @@
 import os, sys
 import pygame
+import threading
+import time
+
+exitFlag = 0
 
 pygame.mixer.pre_init(48000,-16,2,2048)
 pygame.mixer.init()
@@ -9,12 +13,57 @@ pygame.init()
 size = 200,200
 screen = pygame.display.set_mode(size)
 
+class myThread(threading.Thread):
+	def __init__(self, threadID, name, counter):
+		self.threadID - threadID
+		self.name = name
+		self.counter = counter
+	def run(self):
+		print "Starting " + self.name
+		# Synch Threads
+		threadLock.acquire()
+		print_time(self.name, self.counter, 3)
+		threadLock.release()
+		
+def print_time(threadName, delay, counter):
+	while counter:
+		if exitFlag:
+			threadName.exit()
+		time.sleep(delay)
+		print "%s: %s" %(threadName, time.ctime(time.time()))
+		counter -= 1
+		
+threadLock = threading.Lock()
+threads = []
+
+# create threads
+quitThread = myThread(1, "quitThread", 1)
+hihatThread = myThread(2, "hihatThread", 2)
+bassThread = mythread(3, "bassThread", 3)
+snareThread = myThred(4, "snareThread", 4)
+tommidThread = mythread(5, "tommidThread, 5)
+tomlowThread = mythread(6, "tomlowThread", 6)
+crashThread = mythread(7, "creashThread", 7)
+
+#add threads to list
+threads.append(quitThread)
+threads.append(hihatThread)
+threads.append(bassThread)
+threads.append(snareThread)
+threads.append(tommidThread)
+threads.append(tomlowThread)
+threads.append(crashThread)
+
 def playSound(sound):
 	soundLocation = os.path.join("SoundBank",sound+".wav")
 	soundObject = pygame.mixer.Sound(soundLocation)
 	soundObject.play()
 
-startClock = pygame.time.Clock()
+for t in threads:
+	t.join()
+print "Exiting Main Thread"
+
+# startClock = pygame.time.Clock()
 while True:
 	for e in pygame.event.get():
 		if e.type == pygame.QUIT:
@@ -37,4 +86,4 @@ while True:
 				playSound("TomLow")
 		elif e.type == pygame.MOUSEBUTTONDOWN:
 			playSound("Crash")
-	startClock.tick_busy_loop(60)
+	time.sleep(.06)
